b4b00637fb98fd93a52c4bb8c0fd0db4ba0f9287
Arne Coucheron <arco68@gmail.com>
2018-04-20 02:07:10 +0200

    soong: Fix missing print vars for extra features
    Change-Id: Ie20f03acda7d5dbb89262ad5edc1552fcc53c7e2

--- ui/build/make.go
+++ ui/build/make.go
@@ -125,6 +125,8 @@ func runMakeProductConfig(ctx Context, config Config) {
 		"AUX_OS_VARIANT_LIST",
 		"TARGET_BUILD_PDK",
 		"PDK_FUSION_PLATFORM_ZIP",
+		"RECOVERY_VARIANT",
+		"WITH_GMS",
 	}
 
 	allVars := append(append([]string{
